85a9f51b1c633981a1a1c781967e91747cdac5fc
shiv kumar <kumarshiv_sk@outlook.com>
2025-04-09 18:03:43 -0400

    Models file - Employee.cs added and table contents

--- W15bCrudAjaxCodeFirst/Models/Employee.cs
+++ W15bCrudAjaxCodeFirst/Models/Employee.cs
@@ -4,28 +4,27 @@ namespace W15bCrudAjaxCodeFirst.Models
 {
     public class Employee
     {
-        //[Key]     // to make id primary key
-        //public int Id { get; set; }   // properties  - auto type
+        [Key]     // to make id primary key
+        public int Id { get; set; }   // properties  - auto type
 
-        ////[Required(ErrorMessage = "Please enter the Email address")]
-        ////[MaxLength(3, ErrorMessage = "Please enter the Email address")]
+        //[Required(ErrorMessage = "Please enter the Email address")]
+        //[MaxLength(3, ErrorMessage = "Please enter the Email address")]
 
-        //[EmailAddress(ErrorMessage = "Please enter a valid Email address")]
-        //public required string Email { get; set; }
-        //[Required(ErrorMessage = "Please enter the Password")]
-        //public required string Password { get; set; }
+        [EmailAddress(ErrorMessage = "Please enter a valid Email address")]
+        public required string Email { get; set; }
+        [Required(ErrorMessage = "Please enter the Password")]
+        public required string Password { get; set; }
         
-        ////[Required(ErrorMessage = "Please enter the First Name")]
-        ////[MaxLength(3,ErrorMessage = "Please enter the FirstName")]
+        //[Required(ErrorMessage = "Please enter the First Name")]
+        //[MaxLength(3,ErrorMessage = "Please enter the FirstName")]
 
 
-        //[Required (ErrorMessage = "Please enter the FirstName")]
-        //[StringLength(20, MinimumLength = 3,ErrorMessage = "FirstName should be between 3 and 20 characters.")]
-        //public string? FirstName { get; set; }
+        [Required (ErrorMessage = "Please enter the FirstName")]
+        [StringLength(20, MinimumLength = 3,ErrorMessage = "FirstName should be between 3 and 20 characters.")]
+        public string? FirstName { get; set; }
 
+        public string? LastName { get; set; }
 
-        //public string? LastName { get; set; }
-
-        //public string? Address { get; set; }
+        public string? Address { get; set; }
     }
 }
