714a69e3df7c06d6aa3bcd4789de37f358ce4922
shiv kumar <kumarshiv_sk@outlook.com>
2025-04-09 18:02:19 -0400

    DataContext class added Added to Program file

---
+++ W15bCrudAjaxCodeFirst/AppDbContext/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using W15bCrudAjaxCodeFirst.Models;
+
+namespace W15bCrudAjaxCodeFirst.AppDbContext
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }      // Table Name
+    }
+}
---
+++ W15bCrudAjaxCodeFirst/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace W15bCrudAjaxCodeFirst.Models
+{
+    public class Employee
+    {
+        //[Key]     // to make id primary key
+        //public int Id { get; set; }   // properties  - auto type
+
+        ////[Required(ErrorMessage = "Please enter the Email address")]
+        ////[MaxLength(3, ErrorMessage = "Please enter the Email address")]
+
+        //[EmailAddress(ErrorMessage = "Please enter a valid Email address")]
+        //public required string Email { get; set; }
+        //[Required(ErrorMessage = "Please enter the Password")]
+        //public required string Password { get; set; }
+        
+        ////[Required(ErrorMessage = "Please enter the First Name")]
+        ////[MaxLength(3,ErrorMessage = "Please enter the FirstName")]
+
+
+        //[Required (ErrorMessage = "Please enter the FirstName")]
+        //[StringLength(20, MinimumLength = 3,ErrorMessage = "FirstName should be between 3 and 20 characters.")]
+        //public string? FirstName { get; set; }
+
+
+        //public string? LastName { get; set; }
+
+        //public string? Address { get; set; }
+    }
+}
