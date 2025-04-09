3b80ccaf4a0b38c45059feca33c3085295556f61
shiv kumar <kumarshiv_sk@outlook.com>
2025-04-09 17:40:40 -0400

    Add project files.

---
+++ W15bCrudAjaxCodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace W15bCrudAjaxCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
